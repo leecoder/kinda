8e279d2c28f75cc0e3c2ddca34aec695e3e2ae05
Rich Insley <richinsley@gmail.com>
2024-03-28 19:39:25 -0700

    added bound type cmd

--- pkg/pyproc_windows.go
+++ pkg/pyproc_windows.go
@@ -4,16 +4,89 @@
 package pkg
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
 	"os/exec"
+	"os/signal"
 	"syscall"
 )
 
-func setSysProcAttr(cmd *exec.Cmd) {
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
-	}
-}
+func (env *Environment) BoundRunPythonScriptFromFile(scriptPath string, args ...string) error {
+	// Create the command
+	// put scriptPath at the front of the args
+	args = append([]string{scriptPath}, args...)
+	cmd := exec.Command(env.PythonPath, args...)
 
-func terminateChildProcess(cmd *exec.Cmd) {
-	syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+	// Create a pipe for the output of the script
+	stdoutPipe, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		panic(err)
+	}
+
+	// Create a channel to receive signals
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	// Wait for the command to finish or a signal to be received
+	go func() {
+		<-signalChan
+		// Kill the child process when a signal is received
+		cmd.Process.Kill()
+	}()
+
+	// Read from the command's stdout
+	scanner := bufio.NewScanner(stdoutPipe)
+	for scanner.Scan() {
+		fmt.Println("Python script output:", scanner.Text())
+	}
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if exitErr.ProcessState.ExitCode() == 1 {
+				// The child process was killed
+				return errors.New("child process was killed")
+			}
+		}
+		return err
+	}
+
+	return nil
+
+	/*
+		// put scriptPath at the front of the args
+		args = append([]string{scriptPath}, args...)
+		cmd := exec.Command(env.PythonPath, args...)
+
+		// Create a pipe for the output of the script
+		stdoutPipe, err := cmd.StderrPipe()
+		if err != nil {
+			return fmt.Errorf("error creating stdout pipe: %v", err)
+		}
+
+		// Start the command
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+
+		// Read from the command's stdout
+		scanner := bufio.NewScanner(stdoutPipe)
+		for scanner.Scan() {
+			fmt.Println("Python script output:", scanner.Text())
+		}
+
+		// Wait for the command to finish
+		if err := cmd.Wait(); err != nil {
+			return err
+		}
+
+		return nil
+	*/
 }
